a741b7c43d104609e114afa370dd2998ca2f5e37
qs73 <qs73@cornell.edu>
2022-12-01 15:26:27 -0500

    model

--- lab4_modified/ecelinux/model.h
+++ lab4_modified/ecelinux/model.h
@@ -8,20 +8,49 @@
 
 #include "layer.h"
 
-const float w_fc1[3072 * 2000] = {
-#include "data/fc1_weights"
+// 6*3*5*5 = 450
+const float conv1_weight[450] = {
+#include "data/conv1_weight"
 };
 
-const float b_fc1[2000] = {
+const float conv1_bias[6] = {
+#include "data/conv1_bias"
+};
+
+// 16*6*5*5 = 2400
+const float conv2_weight[2400] = {
+#include "data/conv2_weight"
+};
+
+const float conv1_bias[6] = {
+#include "data/conv2_bias"
+};
+
+// 120*400=48000
+const float fc1_weight[48000] = {
+#include "data/fc1_weight"
+};
+
+const float fc1_bias[120] = {
 #include "data/fc1_bias"
 };
 
-const float w_fc2[2000 * 1500] = {
-#include "data/fc2_weights"
+// 84*120=10080
+const float fc2_weight[10080] = {
+#include "data/fc2_weight"
 };
 
-const float b_fc2[1500] = {
+const float fc2_bias[84] = {
 #include "data/fc2_bias"
+};
+
+// 2*84=168
+const float fc3_weight[168] = {
+#include "data/fc3_weight"
+};
+
+const float fc3_bias[2] = {
+#include "data/fc3_bias"
 };
 
 #endif
